31dff84f4fd0861f67c9ca3b648892fc308f2274
Atif <atifkhan308@gmail.com>
2026-03-28 21:22:34 -0400

    Initial Commit

---
+++ Revless/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Revless
+//
+//  Created by Atif Khan on 2026-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Revless/RevlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevlessApp.swift
+//  Revless
+//
+//  Created by Atif Khan on 2026-03-28.
+//
+
+import SwiftUI
+
+@main
+struct RevlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
